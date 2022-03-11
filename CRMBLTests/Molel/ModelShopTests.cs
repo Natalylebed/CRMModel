f8e6500e97e08a0590d8c5c3a74ce93ff6954906
natalebedeva23022013@gmail.com <natalebedeva23022013@gmail.com>
2022-03-11 15:29:53 +0300

    +test

--- CRMBLTests/Molel/ModelShopTests.cs
+++ CRMBLTests/Molel/ModelShopTests.cs
@@ -15,16 +15,14 @@ namespace CRMBL.Molel.Tests
             
            var modelshop = new ModelShop(10, 1000);
            modelshop.Start(3,1,3,2);
+            var actual1 = modelshop.CartInModel.Count;
+            var actual2 = modelshop.CashDesksInModel[0].Queues.Count;
 
-
-            Assert.Fail();
+            Assert.AreEqual(0, actual1);
+            Assert.AreEqual(3, actual2);
         }
 
-        [TestMethod()]
-        public void StartTest()
-        {
-            Assert.Fail();
-        }
+        
 
        
     }
